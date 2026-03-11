4613c7b3e91dc95e2dc519177b0a98c551c67b06
Murtaza Zaidi <zaidimurtaza102@gmail.com>
2026-03-11 21:55:32 +0530

    fix: deployment

--- HRMA_ui/vite.config.js
+++ HRMA_ui/vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    chunkSizeWarningLimit: 1000,
-  },
 })
